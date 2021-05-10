2494c1aba56aa033437621e16076af8eaa4d1012
Davide Angelone <davide.angelone@iad2.it>
2021-05-10 09:39:01 +0200

    updated junit

--- src/test/java/it/test/MongoJwtApplicationTests.java
+++ src/test/java/it/test/MongoJwtApplicationTests.java
@@ -1,7 +1,7 @@
 package it.test;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Set;
 import java.util.stream.Collectors;
