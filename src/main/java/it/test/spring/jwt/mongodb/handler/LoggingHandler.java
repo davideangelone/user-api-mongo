f48b53e89155e8f7f34440879b4b2db05e820899
Davide Angelone <davide.angelone@iad2.it>
2021-05-27 16:24:30 +0200

    no message

--- src/main/java/it/test/spring/jwt/mongodb/handler/LoggingHandler.java
+++ src/main/java/it/test/spring/jwt/mongodb/handler/LoggingHandler.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Aspect
 @Component
